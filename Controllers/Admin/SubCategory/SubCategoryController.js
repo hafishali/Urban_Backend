edef6666cd606709ebc9347cd6bfe87306742fc7
hafishali <hafishali4@gmail.com>
2024-12-04 15:56:43 +0530

    subcategory creation

--- Controllers/Admin/SubCategory/SubCategoryController.js
+++ Controllers/Admin/SubCategory/SubCategoryController.js
@@ -1,1 +1,19 @@
 const SubCategory=require('../../../Models/Admin/SubcategoyModel')
+
+
+// create subcategory
+exports.createSubCategory=async(req,res)=>{
+    const{title,category,image}=req.body
+        if(!req.file){
+            return res.status(400).json({message:"SubCategory Image is required"})
+        }
+    try {
+       const newSubCategory=new SubCategory({title,category,image: req.file.filename}) 
+        newSubCategory.save()
+        res.status(201).json({ message: 'SubCategory created successfully' , SubCategory: newSubCategory});
+    } catch (error) {
+        res.status(500).json({message:"Internal Server Error",error})
+    }
+}
+
+// get subcategory
