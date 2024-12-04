a4ea6d6de1819a701a1f5f021cd4607766f804a9
Amith Krishnan <amithkrishnan20@gmail.com>
2024-12-04 14:50:45 +0530

    change in category model

--- Models/Admin/CategoryModel.js
+++ Models/Admin/CategoryModel.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const categorySchema = new mongoose.Schema({
     name: {
         type: String,
-        required: true,
+        required: [true, "Name is required"],
         unique: true, 
     },
     image: {
