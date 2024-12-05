154e625427cc37e0fdf7dcbf77e3b9ebb15edf86
hafishali <hafishali4@gmail.com>
2024-12-05 10:30:32 +0530

    compeletd subcategory

--- Controllers/Admin/SubCategory/SubCategoryController.js
+++ Controllers/Admin/SubCategory/SubCategoryController.js
@@ -1,19 +1,119 @@
-const SubCategory=require('../../../Models/Admin/SubcategoyModel')
-
+const SubCategory = require('../../../Models/Admin/SubcategoyModel')
+const fs = require('fs');
 
 // create subcategory
-exports.createSubCategory=async(req,res)=>{
-    const{title,category,image}=req.body
-        if(!req.file){
-            return res.status(400).json({message:"SubCategory Image is required"})
-        }
+exports.createSubCategory = async (req, res) => {
+    const { title, category, image } = req.body
+    if (!req.file) {
+        return res.status(400).json({ message: "SubCategory Image is required" })
+    }
     try {
-       const newSubCategory=new SubCategory({title,category,image: req.file.filename}) 
-        newSubCategory.save()
-        res.status(201).json({ message: 'SubCategory created successfully' , SubCategory: newSubCategory});
+        const newSubCategory = new SubCategory({ title, category, image: req.file.filename })
+        await newSubCategory.save()
+        res.status(201).json({ message: 'SubCategory created successfully', SubCategory: newSubCategory });
     } catch (error) {
-        res.status(500).json({message:"Internal Server Error",error})
+        res.status(500).json({ message: "Internal Server Error", error })
     }
 }
 
 // get subcategory
+exports.getSubCategories = async (req, res) => {
+    try {
+        const subcategory = await SubCategory.find().populate('category')
+
+        const SubcategoriesWithImageUrl = subcategory.map((subcategories) => ({
+            id: subcategories._id,
+            title: subcategories.title,
+            MainCategory: {
+                id: subcategories.category?._id,
+                name: subcategories.category?.name,
+                description: subcategories.category?.description,
+                imageUrl: `${req.protocol}://${req.get('host')}/uploads/category/${subcategories.category?.image}`
+            },
+            SubImageUrl: `${req.protocol}://${req.get('host')}/uploads/category/${subcategories.image}`
+        }))
+        res.status(200).json(SubcategoriesWithImageUrl);
+        // res.status(200).json(subcategory);
+    } catch (err) {
+        console.log(err)
+        res.status(500).json({ message: 'Error fetching Sub categories', error: err.message });
+    }
+}
+
+// get subcategorybyId
+exports.getSubCategoryById = async (req, res)=> {
+    const { id } = req.params;
+    try {
+        const subcategory = await SubCategory.findById(id).populate('category')
+        if(!subcategory) {
+            return res.status(404).json({ message: 'SubCategory not found' });
+        }   
+        const SubcategoryWithImageUrl = {
+            id: subcategory._id,
+            title: subcategory.title,
+            MainCategory: {
+                id: subcategory.category?._id,
+                name: subcategory.category?.name,
+                description: subcategory.category?.description,
+                imageUrl: `${req.protocol}://${req.get('host')}/uploads/category/${subcategory.category?.image}`
+            },
+            imageUrl: `${req.protocol}://${req.get('host')}/uploads/category/${subcategory.image}`
+        };
+        res.status(200).json(SubcategoryWithImageUrl);
+    } catch (err) {
+        res.status(500).json({ message: 'Error fetching Subcategory', error: err.message });
+    }
+}
+
+// update category
+exports.updateSubCategory = async (req, res) => {
+    const { id } = req.params;
+    const updates = req.body; 
+    try {
+        if (req.file) {
+            const subcategory = await SubCategory.findById(id);
+            if (subcategory && subcategory.image) {
+                const oldImagePath = `./uploads/category/${subcategory.image}`;
+                if (fs.existsSync(oldImagePath)) {
+                    fs.unlinkSync(oldImagePath);
+                }
+            }
+            updates.image = req.file.filename;
+        }
+        const updatedSubCategory = await SubCategory.findByIdAndUpdate(id, updates, {
+            new: true, 
+            runValidators: true, 
+        });
+        if (!updatedSubCategory) {
+            return res.status(404).json({ message: 'SubCategory not found' });
+        }
+        res.status(200).json({ message: 'SubCategory updated successfully', updatedSubCategory });
+    } catch (err) {
+        res.status(500).json({ message: 'Error updating SubCategory', error: err.message });
+    }
+};
+
+// delete subcategory
+exports.deleteSubCategory = async (req,res) => {
+    const { id } = req.params;
+
+    try {
+        const subcategory = await SubCategory.findById(id);
+
+        if(!subcategory) {
+            return res.status(404).json({ message: 'SubCategory not found' });
+        }
+
+        // delete the image file
+        const imagePath = `./uploads/category/${subcategory.image}`
+        if (fs.existsSync(imagePath)) {
+            fs.unlinkSync(imagePath);
+        }
+
+        // delete the category from the database
+        await SubCategory.findByIdAndDelete(id);
+        res.status(200).json({ message: 'SubCategory deleted successfully' });
+    } catch (err) {
+        res.status(500).json({ message: 'Error deleting Subcategory', error: err.message });
+    }
+};
