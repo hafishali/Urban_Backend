6433a4bf916c21fdfacfa797fb267c1ea8dcecbe
hafishali <hafishali4@gmail.com>
2024-12-06 10:06:53 +0530

    updated jwtAuth based on roles

--- Routes/Admin/Slider/sliderRoute.js
+++ Routes/Admin/Slider/sliderRoute.js
@@ -6,15 +6,15 @@ const jwtVerify=require('../../../Middlewares/jwtMiddleware')
 
 
 // create a new slider
-router.post('/create', jwtVerify, multer.single('image'), SliderController.createSlider);
+router.post('/create', jwtVerify(['admin']), multer.single('image'), SliderController.createSlider);
 
 // get all sliders
 router.get('/', SliderController.getAllSliders)
 
 // update slider
-router.patch('/:id', jwtVerify, multer.single('image'), SliderController.updateSlider);
+router.patch('/:id', jwtVerify(['admin']), multer.single('image'), SliderController.updateSlider);
 
 // delete slider
-router.delete('/:id', jwtVerify, SliderController.deleteSlider);
+router.delete('/:id', jwtVerify(['admin']), SliderController.deleteSlider);
 
 module.exports = router;
