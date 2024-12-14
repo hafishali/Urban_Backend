b4766c752fb6274285b98df20f1012ba6f5cfcb7
Kajahussain123 <kajahusain513@gmail.com>
2024-12-14 16:42:04 +0530

    updates

--- utils/generateNumericOrderId.js
+++ utils/generateNumericOrderId.js
@@ -1,10 +1,10 @@
 const Counter = require('../Models/User/CounterModel');
 async function generateNumericOrderId() {
   const counter = await Counter.findOneAndUpdate(
-    { name: 'orderId' }, // Specify the counter name
-    { $inc: { seq: 1 } }, // Increment the sequence by 1
-    { new: true, upsert: true } // Create the counter document if it doesn't exist
+    { name: 'orderId' }, 
+    { $inc: { seq: 1 } }, 
+    { new: true, upsert: true } 
   );
-  return counter.seq; // Return the updated sequence number
+  return counter.seq;
 }
 module.exports = generateNumericOrderId;
