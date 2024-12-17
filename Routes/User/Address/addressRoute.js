577ff7286ddfb136167f19082ea806adf7e8697f
Kajahussain123 <kajahusain513@gmail.com>
2024-12-17 10:34:39 +0530

    updates

--- Routes/User/Address/addressRoute.js
+++ Routes/User/Address/addressRoute.js
@@ -1,17 +1,19 @@
 const express = require('express');
 const addressController = require('../../../Controllers/User/Address/addressController')
 const router = express.Router();
+const jwtVerify=require('../../../Middlewares/jwtMiddleware')
+
 
 // Add a new address
-router.post('/add', addressController.addAddress);
+router.post('/add', jwtVerify(['user']), addressController.addAddress);
 
 // Get all addresses for a user
-router.get('/view/:userId', addressController.getAddressesByUserId);
+router.get('/view/:userId', jwtVerify(['user']), addressController.getAddressesByUserId);
 
 // Update an address by ID
-router.put('/update/:id', addressController.updateAddress);
+router.put('/update/:id', jwtVerify(['user']), addressController.updateAddress);
 
 // Delete an address by ID
-router.delete('/delete/:id', addressController.deleteAddress);
+router.delete('/delete/:id', jwtVerify(['user']), addressController.deleteAddress);
 
 module.exports = router;
--- Routes/User/Order/OrderRoute.js
+++ Routes/User/Order/OrderRoute.js
@@ -1,14 +1,16 @@
 const express = require('express');
 const router = express.Router();
 const orderController = require('../../../Controllers/User/Order/orderController');
+const jwtVerify=require('../../../Middlewares/jwtMiddleware')
+
 
 // Place an order
-router.post('/create', orderController.placeOrder);
+router.post('/create', jwtVerify(['user']), orderController.placeOrder);
 
 // Get orders for a user
-router.get('/view/:userId', orderController.getUserOrders);
+router.get('/view/:userId', jwtVerify(['user']), orderController.getUserOrders);
 
 // Get order details by ID
-router.get('/orders/:orderId', orderController.getOrderById);
+router.get('/orders/:orderId', jwtVerify(['user']), orderController.getOrderById);
 
 module.exports = router;
